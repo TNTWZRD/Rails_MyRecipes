fbca38e88672ee915a4e7a92f66252b4753b7182
TNTWZRD <danieljajliardo@gmail.com>
2015-12-15 18:28:29 +0000

    Added Recipe sql size and existance reqiorements

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
   
+  validates :name, presence: true, length: 5..100
+  validates :summary, presence: true, length: 10..150
+  validates :description, presence: true, length: 20..500
+  
 end
