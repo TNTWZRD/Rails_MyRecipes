745f64fdb5554c98598e34fd5306538bacc5422e
TNTWZRD <danieljajliardo@gmail.com>
2015-12-15 20:29:09 +0000

    Added Gravitar Support and recipe viewing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def gravitar_for(chef, options = { size: 80 })
+    gravitar_id = Digest::MD5::hexdigest(chef.email.downcase)
+    size = options[:size]
+    gravitar_url = "https://secure.gravatar.com/avatar/#{gravitar_id}?s=#{size}"
+    image_tag(gravitar_url, alt: chef.name, class: "gravitar");
+  end
+  
 end
