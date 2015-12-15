16c7b8421482106909a6b08734e886134e47d2c7
TNTWZRD <danieljajliardo@gmail.com>
2015-12-15 19:33:21 +0000

    Created Tests, and modifyed recipe Database

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,10 @@
 class Recipe < ActiveRecord::Base
   
+  belongs_to :chef
+  
   validates :name, presence: true, length: 5..100
   validates :summary, presence: true, length: 10..150
   validates :description, presence: true, length: 20..500
+  validates :chef_id, presence: true
   
 end
